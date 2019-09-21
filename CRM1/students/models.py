7da23172c22e9ac8485cab3724d10c8f25b9b22e
lin <lin@qq.com>
2019-09-22 07:27:24 +0800

    commit

--- CRM1/students/models.py
+++ CRM1/students/models.py
@@ -4,12 +4,40 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=20)
-    age = models.SmallIntegerField()
-    sex = models.SmallIntegerField(max_length=1)
-    qq = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
+    age = models.SmallIntegerField(null=True)
+    sex = models.SmallIntegerField(default=1)
+    qq = models.CharField(max_length=20, unique=True)
+    phone = models.CharField(max_length=20, unique=True)
     c_time = models.DateTimeField(auto_now_add=True)
+    grade = models.ForeignKey('Grade', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
+class StudentDetail(models.Model):
+    college = models.CharField(max_length=20)
+    student = models.OneToOneField('Student', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.college
+
+class Grade(models.Model):
+    name = models.CharField(max_length=20)
+    num = models.CharField('班期', max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    students = models.ManyToManyField('Student', through='Enroll')
+
+    def __str__(self):
+        return self.name
+
+# 中间表
+class Enroll(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    pay = models.FloatField('缴费金额', default=0)
+    c_time = models.DateTimeField('报名时间', auto_now_add=True)
---
+++ 笔记/1_虚拟环境命令，django命令.md
@@ -0,0 +1,22 @@
+### 虚拟环境命令
+    
+    mkvirtualenv -p python解释器路径 虚拟环境名
+    rmvirtualenv 虚拟环境名
+    
+    workon 虚拟环境名
+    deactivate
+    
+    
+### django命令
+
+    startproject
+    startapp
+    runserver
+    shell
+    admin
+    
+    makemigration
+    migrate
+    dumpdata
+    loaddata
+    
