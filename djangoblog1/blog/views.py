bac6a9fde2ff88e6995b3960ae9841798ad1fdbb
lin <lin@qq.com>
2019-10-24 09:29:58 +0800

    commit

---
+++ djangoblog1/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html', context={
+
+    })
+
+def home(request):
+    return render(request, 'blog/home.html', context={
+
+    })
